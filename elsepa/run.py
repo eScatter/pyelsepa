0f8b0e9ffbaa577159a050527a790e8645d140c9
Sebastiaan Lokhorst <s.r.lokhorst@student.tudelft.nl>
2017-04-20 13:06:05 +0200

    Add option to run a local ELSEPA instance, without Docker Do this by setting the ELSEPA_DIR environment variable.

--- elsepa/run.py
+++ elsepa/run.py
@@ -3,30 +3,54 @@ from elsepa.parse_output import (elsepa_output_parsers)
 from elsepa.executable import (DockerContainer, Archive)
 
 import re
+import os
+import glob
+import subprocess
 
 
 def elscata(settings: Settings):
-    with DockerContainer('elsepa', working_dir='/opt/elsepa') as elsepa:
-        elsepa.put_archive(
-            Archive('w')
-            .add_text_file('input.dat', generate_elscata_input(settings))
-            .close())
+    elsepa_dir = os.getenv('ELSEPA_DIR')
+    result = {}
 
-        elsepa.sh('./elscata < input.dat',
-                  'mkdir result && mv *.dat result')
+    if elsepa_dir:
+        os.chdir(elsepa_dir)
+        for fn in glob.glob('*.dat'):
+            os.remove(fn)
 
-        result_tar = elsepa.get_archive('result')
-        result = {}
+        subprocess.run('./elscata',
+                       input=bytes(generate_elscata_input(settings), 'utf-8'),
+                       stdout=subprocess.DEVNULL)
 
-        for info in result_tar:
-            if not info.isfile():
-                continue
-
-            name = re.match("result/(.*)\\.dat", info.name).group(1)
+        for fn in glob.glob('*.dat'):
+            name = re.match("(.*)\\.dat", fn).group(1)
             parser = elsepa_output_parsers[name]
 
             if parser:
-                lines = result_tar.get_text_file(info).split('\n')
+                with open(fn, "r") as f:
+                    lines = f.readlines()
                 result[name] = parser(lines)
+
+    else:
+        with DockerContainer('elsepa', working_dir='/opt/elsepa') as elsepa:
+            elsepa.put_archive(
+                Archive('w')
+                .add_text_file('input.dat', generate_elscata_input(settings))
+                .close())
+
+            elsepa.sh('./elscata < input.dat',
+                      'mkdir result && mv *.dat result')
+
+            result_tar = elsepa.get_archive('result')
+
+            for info in result_tar:
+                if not info.isfile():
+                    continue
+
+                name = re.match("result/(.*)\\.dat", info.name).group(1)
+                parser = elsepa_output_parsers[name]
+
+                if parser:
+                    lines = result_tar.get_text_file(info).split('\n')
+                    result[name] = parser(lines)
 
     return result
