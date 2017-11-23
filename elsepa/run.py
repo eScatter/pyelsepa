930aa16ba7098a7b2ea0d47a4e86d675d00066a0
Luc van Kessel <L.C.P.M.vanKessel@tudelft.nl>
2017-11-23 16:19:39 +0100

    Add ability to run elsepa without docker
    Uses a temporary working directory for elsepa, making it possible to run multiple instances in parallel. Does require the modified version (github.com/eScatter/elsepa) of elsepa, to take the ELSEPA_DATA environment variable.

--- elsepa/run.py
+++ elsepa/run.py
@@ -3,9 +3,40 @@ from elsepa.parse_output import (elsepa_output_parsers)
 from elsepa.executable import (DockerContainer, Archive)
 
 import re
+import os
+import glob
+import subprocess
+import tempfile
 
+# Run elscata using subprocess
+def elscata(settings: Settings, elsepa_dir='/opt/elsepa/', elsepa_data_dir='/opt/elsepa/data/'):
+    result = {}
 
-def elscata(settings: Settings):
+    environment = os.environ.copy()
+    environment['ELSEPA_DATA'] = elsepa_data_dir
+
+    elscata_binary = os.path.join(elsepa_dir, 'elscata')
+
+    with tempfile.TemporaryDirectory() as elsepa_output_dir:
+        subprocess.run(elscata_binary,
+                       input=bytes(generate_elscata_input(settings), 'utf-8'),
+                       stdout=subprocess.DEVNULL,
+                       cwd=elsepa_output_dir,
+                       env=environment)
+
+        for fn in glob.glob(os.path.join(elsepa_output_dir, '*.dat')):
+            name = re.match("(.*)\\.dat", os.path.basename(fn)).group(1)
+            parser = elsepa_output_parsers[name]
+
+            if parser:
+                with open(fn, "r") as f:
+                    lines = f.readlines()
+                result[name] = parser(lines)
+
+    return result
+
+# Run elscata from a docker
+def elscata_docker(settings: Settings):
     with DockerContainer('elsepa', working_dir='/opt/elsepa') as elsepa:
         elsepa.put_archive(
             Archive('w')
