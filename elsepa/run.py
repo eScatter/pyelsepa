37a876b3a35b6f710846ecde8277e8021e1b8ba3
Luc van Kessel <L.C.P.M.vanKessel@tudelft.nl>
2017-11-24 10:53:00 +0100

    Really remove all dependencies on Docker
    Forgot the most important change in my previous commit...

--- elsepa/run.py
+++ elsepa/run.py
@@ -1,6 +1,5 @@
 from elsepa.generate_input import (generate_elscata_input, Settings)
 from elsepa.parse_output import (elsepa_output_parsers)
-from elsepa.executable import (DockerContainer, Archive)
 
 import re
 import os
@@ -8,7 +7,6 @@ import glob
 import subprocess
 import tempfile
 
-# Run elscata using subprocess
 def elscata(settings: Settings, elsepa_dir='/opt/elsepa/', elsepa_data_dir='/opt/elsepa/data/'):
     result = {}
 
@@ -31,33 +29,6 @@ def elscata(settings: Settings, elsepa_dir='/opt/elsepa/', elsepa_data_dir='/opt
             if parser:
                 with open(fn, "r") as f:
                     lines = f.readlines()
-                result[name] = parser(lines)
-
-    return result
-
-# Run elscata from a docker
-def elscata_docker(settings: Settings):
-    with DockerContainer('elsepa', working_dir='/opt/elsepa') as elsepa:
-        elsepa.put_archive(
-            Archive('w')
-            .add_text_file('input.dat', generate_elscata_input(settings))
-            .close())
-
-        elsepa.sh('./elscata < input.dat',
-                  'mkdir result && mv *.dat result')
-
-        result_tar = elsepa.get_archive('result')
-        result = {}
-
-        for info in result_tar:
-            if not info.isfile():
-                continue
-
-            name = re.match("result/(.*)\\.dat", info.name).group(1)
-            parser = elsepa_output_parsers[name]
-
-            if parser:
-                lines = result_tar.get_text_file(info).split('\n')
                 result[name] = parser(lines)
 
     return result
