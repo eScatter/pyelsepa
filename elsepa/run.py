243550ec5ca4036aa15ff8a2aa91e6571ac4359d
Luc van Kessel <L.C.P.M.vanKessel@tudelft.nl>
2017-11-24 10:57:18 +0100

    Find elsepa directories in a more intelligent way

--- elsepa/run.py
+++ elsepa/run.py
@@ -4,24 +4,41 @@ from elsepa.parse_output import (elsepa_output_parsers)
 import re
 import os
 import glob
+import shutil
 import subprocess
 import tempfile
 
-def elscata(settings: Settings, elsepa_dir='/opt/elsepa/', elsepa_data_dir='/opt/elsepa/data/'):
+# Run elscata.
+# By default, the ELSEPA path is found from $PATH and the data folder
+# is found from the ELSEPA_DATA environment variable.
+# They may be overridden by providing the last two parameters.
+def elscata(settings: Settings, elsepa_dir=None, elsepa_data_dir=None):
+   # Get path to the elscata binary and data directory.
+    elscata_binary = shutil.which('elscata') if elsepa_dir is None \
+                     else os.path.join(elsepa_dir, 'elscata')
+    elsepa_data_dir = os.environ.get('ELSEPA_DATA') if elsepa_data_dir is None \
+                      else elsepa_data_dir
+
+    # Check if they are actually accessible
+    if not os.path.isfile(elscata_binary):
+        raise FileNotFoundError('Unable to find the elscata binary')
+    if not os.path.isfile(os.path.join(elsepa_data_dir, 'z_001.den')):
+        raise FileNotFoundError('Unable to find the ELSEPA data directory')
+
     result = {}
 
-    environment = os.environ.copy()
-    environment['ELSEPA_DATA'] = elsepa_data_dir
-
-    elscata_binary = os.path.join(elsepa_dir, 'elscata')
-
     with tempfile.TemporaryDirectory() as elsepa_output_dir:
+        # Run elscata
+        elscata_environment = os.environ.copy()
+        elscata_environment['ELSEPA_DATA'] = elsepa_data_dir
+
         subprocess.run(elscata_binary,
                        input=bytes(generate_elscata_input(settings), 'utf-8'),
                        stdout=subprocess.DEVNULL,
                        cwd=elsepa_output_dir,
-                       env=environment)
+                       env=elscata_environment)
 
+        # Collect output
         for fn in glob.glob(os.path.join(elsepa_output_dir, '*.dat')):
             name = re.match("(.*)\\.dat", os.path.basename(fn)).group(1)
             parser = elsepa_output_parsers[name]
